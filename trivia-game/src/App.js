6ef5a303790390951c6e416a2abbae0337864acf
PretzelOptician <alekschultz1@gmail.com>
2022-04-18 00:43:38 -0400

    css changes

--- trivia-game/src/App.js
+++ trivia-game/src/App.js
@@ -3,18 +3,19 @@ import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid'
 import './App.css';
 import React from 'react';
-import styleFunctionSx from '@mui/system/styleFunctionSx';
 
 class App extends React.Component {
   constructor(props) {
     super(props); 
     this.state = {
       items: [], 
-      DataisLoaded: false
+      DataisLoaded: false,
+      incorrectScore: 0, 
+      correctScore: 0,
     };
   }
   componentDidMount() {
-    fetch("https://opentdb.com/api.php?amount=10")
+    fetch("https://opentdb.com/api.php?amount=20")
         .then((res) => res.json())
         .then((json) => {
             this.setState({
@@ -29,11 +30,39 @@ class App extends React.Component {
     else {
       console.log(items); 
       return (
-        <div className="App">
-          {items.map((item) => <TriviaCard key={item.question} item={item}/>)}
+        <div className="App" style={{display: "flex", backgroundColor: '#D3D3D3'}}>
+          <StatsCard correctScore={this.state.correctScore} incorrectScore={this.state.incorrectScore} />
+          <TriviaGrid items={items} correct={this.state.correctScore} correctCounter={() => this.setState({correctScore: this.state.correctScore+1})} incorrect={this.state.incorrectScore} incorrectCounter={() => this.setState({incorrectScore: this.state.incorrectScore+1})}/>
         </div>
       );
     }
+  }
+}
+
+class TriviaGrid extends React.Component {
+  render() {
+    return(
+      <div>
+        {this.props.items.map((item) => <TriviaCard key={item.question} item={item} correctCounter={() => this.props.correctCounter()} incorrectCounter={() => this.props.incorrectCounter()}/>)}
+      </div>
+    );
+  }
+}
+
+class StatsCard extends React.Component {
+  render() {
+    let percent = 0; 
+    if(this.props.incorrectScore > 0) {
+      percent = this.props.correctScore*100/(this.props.incorrectScore+this.props.correctScore);
+    }
+    return ( 
+      <div style={{borderRight: "dotted", borderWidth: "6px", width: "300px", backgroundColor: "#daf0e3", padding: "6px"}}>
+        <h1 style={{fontFamily: "Courier New, monospace"}}>Statistics</h1>
+        <h5>Questions Correct: {this.props.correctScore}</h5>
+        <h5>Questions Incorrect: {this.props.incorrectScore} </h5>
+        <h5>Percentage: {percent}%</h5>
+      </div>
+    );
   }
 }
 
@@ -54,81 +83,60 @@ class TriviaCard extends React.Component {
   }
   fix(string) {
     let newString = string; 
-    //while(newString.includes('&quot;')) { 
-      newString = newString.replace(/&quot;/g, '"'); 
-    //}
-    //while(newString.includes('&#039;')) {
+    newString = newString.replace(/&quot;/g, '"'); 
     newString = newString.replace(/&#039;/g, "'"); 
-    //}
     return newString;
   }
   render() {
+    const divStyle = {
+      padding: "10px", 
+      margin: "23px", 
+      marginLeft: "30px",
+      borderLeft: "solid", 
+      borderColor: "lightblue", 
+      borderWidth: "6px", 
+      backgroundColor: "white",
+    }
+    const buttonStyle = {
+      margin: "5px",
+    }
     if(!this.state.disabled) {
       return(
-        <div>
+        <div style={divStyle}>
           <h3>{this.fix(this.props.item.question)}</h3>
-          {this.state.answers.map((answer) => <Button variant="outlined" onClick={() => {
+          {this.state.answers.map((answer) => <Button style={buttonStyle} variant="outlined" onClick={() => {
             if(answer == this.state.correct) {
               this.setState({disabled: true, correct:true});
+              this.props.correctCounter(); 
               console.log("correct");
             }
             else {
               this.setState({disabled: true, correct:false});
+              this.props.incorrectCounter(); 
               console.log("incorrect"); 
             }
           }}> {this.fix(answer)} </Button>)}
-          <AnswerGrid answers={this.state.answers} />
         </div>
       );
     }
     else if(!this.state.correct) {
       return(
-        <div>
+        <div style={divStyle}>
           <h3 style={{color: "red"}}>{this.fix(this.props.item.question)}</h3>
-          {this.state.answers.map((answer) => <Button variant="outlined" disabled> {this.fix(answer)} </Button>)}
+          {this.state.answers.map((answer) => <Button style={buttonStyle} variant="outlined" disabled> {this.fix(answer)} </Button>)}
         </div>
       );
     }
     else{
       return(
-        <div>
+        <div style={divStyle}>
           <h3 style={{color: 'green'}}>{this.fix(this.props.item.question)}</h3>
-          {this.state.answers.map((answer) => <Button variant="outlined" disabled> {this.fix(answer)} </Button>)}
+          {this.state.answers.map((answer) => <Button style={buttonStyle} variant="outlined" disabled> {this.fix(answer)} </Button>)}
         </div>
       );
     }
   }
 }
 
-class AnswerGrid extends React.Component {
-  render() {
-  return(
-    <Grid container spacing={2}>
-  <Grid item xs={3}>
-    <Button>{this.props.answers[0]}</Button>
-  </Grid>
-  <Grid item xs={3}>
-    <Button>{this.props.answers[1]}</Button>
-  </Grid>
-  <Grid item xs={3}>
-    <Button>{this.props.answers[2]}</Button>
-  </Grid>
-  <Grid item xs={3}>
-    <Button variant="outlined">{this.props.answers[3]}</Button>
-  </Grid>
-    </Grid>
-  );
-  }
-}
-
-class TriviaButton extends React.Component {
-  render() {
-    return(
-      <div>
-        <Button variant="outlined"> {this.props.answer} </Button>
-      </div>
-    );
-  }
-}
 
 export default App;
